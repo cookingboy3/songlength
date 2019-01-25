10503cf698e200a6650582caf0b7b58a15c5f8d7
Justin Lemke <jlemke@tigard.tech>
2019-01-25 13:28:38 -0800

    feature update: add stemplots, verbosity controls

--- songlength.py
+++ songlength.py
@@ -8,6 +8,8 @@ parser = argparse.ArgumentParser(description="get the lengths for a CSV list of 
 parser.add_argument('file', help='CSV to load songs from')
 parser.add_argument('--startAt', help='row to start processing at in the CSV', action='store', type=int)
 parser.add_argument('--apikey', help='API key to use for this run - set this more permanently by editing the secrets file.', action='store')
+parser.add_argument('-sp', '--stemplot', help='Generate a stem plot of the song lengths while collecting data.', action='store_true')
+parser.add_argument('-v', '--verbose', help='Ruin your pretty table by dumping debug info to stdout.', action='store_true')
 args = parser.parse_args()
 
 apikey = ""
@@ -34,18 +36,51 @@ def getSongInfo(track, artist):
     headers=Const.useragent)
     return r.json()
 
+class Stemplot:
+    internalPlot = {}
+    def addData(self, number):
+        if not args.stemplot: return
+        dbgPrint(f"adding {number} to stemplot")
+        # stringify the number so we can do splitting on it because I'm too lazy to use mod
+        number = str(number)
+        leftSplit = int(number[0:2])
+        rightSplit = int(number[2:3])
+        dbgPrint(f"number splits as {leftSplit}|{rightSplit}")
+        if leftSplit not in self.internalPlot:
+            dbgPrint(f"added new list to dict for {leftSplit}.")
+            self.internalPlot[leftSplit] = []
+        self.internalPlot[leftSplit].append(rightSplit)
+
+    def dumpPlot(self):
+        dbgPrint("presorting plot lists")
+        for key in iter(self.internalPlot):
+            self.internalPlot[key] = sorted(self.internalPlot[key])
+
+        dbgPrint("dumping the plot")
+        for key in iter(sorted(self.internalPlot)):
+            print(f"{key} | ", end='')
+            for value in self.internalPlot[key]:
+                print(f"{value} ", end='')
+            print("")
+
+def dbgPrint(string):
+    if args.verbose:
+        print(string)
+
 # real code
 file = args.file
 outfile = time.strftime("output-%d%m%y-%H%M.csv", time.localtime())
 print("started songlength.py")
 print("opening flie {} as read-only".format(file))
 print("outputting to file {}".format(outfile))
+if args.stemplot: print("generating stemplot for data")
 with open(file, 'r', newline='') as oldlist:
     with open(outfile, 'w', newline='') as newlist:
         try:
             songreader = csv.reader(oldlist, dialect='excel')
             songwriter = csv.writer(newlist, dialect='excel')
             rowcount = 0
+            stem = Stemplot()
             for row in songreader:
 
                 if args.startAt != None:
@@ -62,17 +97,23 @@ with open(file, 'r', newline='') as oldlist:
                     track = si['track']['name']
                     artist = si['track']['artist']['name']
                     duration = int(si['track']['duration']) / 1000
-                    print("{:3d} | {:>40} | {:>30} | {}:{:02.0f}".format(rowcount, track, artist, math.ceil(duration/60), duration%60))
+                    print("{:3d} | {:>40} | {:>30} | {}:{:02.0f} | {}".format(rowcount, track, artist, math.ceil(duration/60), duration%60, duration))
                     if int(si['track']['duration']) < 1:
-                        print("not recording this song, it has no duration.")
+                        dbgPrint("not recording this song, it has no duration.")
                     else:
-                        songwriter.writerow([track, artist, duration])
-                    time.sleep(0.5)
+                        songwriter.writerow([track, artist, duration, track[0]])
+                        # hacking some stem plot code on to this
+                        stem.addData(duration)
+                        # end hacky stem plot code
+                    time.sleep(0.2)
                 rowcount += 1
+            stem.dumpPlot()
         except Exception:
             print("HIT EXCEPTION!")
+            print('TRACEBACK -------------------------------------------')
             exinfo = sys.exc_info()
             sys.excepthook(exinfo[0], exinfo[1], exinfo[2])
+            print('END TRACEBACK ---------------------------------------')
             print(si)
             print(row)
             sys.exit(1)
